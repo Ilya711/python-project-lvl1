c0c5842cf51315bf6264a8457f215ec63f9f8a23
root <root@Bender-.localdomain>
2020-01-29 15:39:04 +0300

    добавление комментариев.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,9 @@
+#Скрипт вызова
+from brain_games.cli import welcome_user 
+
 def welcome():
     print('Welcome to the Brain Games!')
 
 if __name__ == '__main__':
     welcome()
+    welcome_user()
