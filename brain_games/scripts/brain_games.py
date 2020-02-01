3f2ed400753e1d9f0c2a98815c1fe17d5ed88a7b
root <root@Bender-.localdomain>
2020-02-01 20:39:26 +0300

    Добавление CodeClimate, flake8, TravisCI. Шаг четвертый.

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,7 @@
-#Скрипт вызова
 from brain_games.cli import welcome_user 
 
 def welcome():
     print('Welcome to the Brain Games!')
-
 if __name__ == '__main__':
     welcome()
     welcome_user()
